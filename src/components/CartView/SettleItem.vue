787e1463164624786a8c0a2ae2df8557c4013eca
No06 <2746419228@qq.com>
2023-12-05 17:22:54 +0800

    为购物单的条目显示购买件数

--- src/components/CartView/SettleItem.vue
+++ src/components/CartView/SettleItem.vue
@@ -15,10 +15,17 @@ defineProps({
 </script>
 
 <template>
-    <div class="d-flex">
+    <div class="d-flex w-100">
         <h3 class="pr-2">{{ index }}.</h3>
-        <div>
-            <h3>{{ cart.product.name }}</h3>
+        <div class="w-100">
+            <v-row no-gutters>
+                <v-col>
+                    <h3>{{ cart.product.name }}</h3>
+                </v-col>
+                <div class="d-flex align-end">
+                    <h3 style="color:darkgrey;">x {{ cart.quantity }}</h3>
+                </div>
+            </v-row>
             <h4 style="color: grey;">
                 {{ cart.product.price }} x {{ cart.quantity }} = ￥{{ cart.subtotal }}
             </h4>
